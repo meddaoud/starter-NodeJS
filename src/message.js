32c92791bc4b7cf6981ca02de487ae1b6bfc5553
meddaoud <31484452+meddaoud@users.noreply.github.com>
2017-09-04 01:44:23 +0200

    Update message.js

--- src/message.js
+++ src/message.js
@@ -33,7 +33,7 @@ const replyMessage = (message) => {
 
     // If there is not any message return by Recast.AI for this current conversation
     if (!result.replies.length) {
-      message.addReply({ type: 'text', content: 'I don\'t have the reply to this yet :)' })
+      message.addReply({ type: 'text', content: 'Ma fhemtech' })
     } else {
       // Add each reply received from API to replies stack
       result.replies.forEach(replyContent => message.addReply({ type: 'text', content: replyContent }))
