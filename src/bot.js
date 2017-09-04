bb805d480c004caeabdcd3bf78047fa54e93402b
meddaoud <31484452+meddaoud@users.noreply.github.com>
2017-09-04 02:31:45 +0200

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -76,15 +76,3 @@ export const bot = (body, response, callback) => {
     callback('No text provided')
   }
 }
-
-curl -X POST -H "Content-Type: application/json" -d '{
-  "greeting":[
-    {
-      "locale":"default",
-      "text":"Hello!"
-    }, {
-      "locale":"en_US",
-      "text":"Timeless apparel for the masses."
-    }
-  ] 
-}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAV2SPNdsF0BALOT3tnFZC2qRjNx2FR8quSZBPtCG0ZAZC1zxnelgB6nCtnmZBixy4XZCN0uDaPXica5SzuAUcBGHT5e2kYyyiKZBdaIBommGdKkDsS5mR8lMUT9byKGVfYfQ1TSTwxgxDi01DyQqpIUWKtdicHwp1A6vyo3Ws5mD1ruPP8XXMM"
