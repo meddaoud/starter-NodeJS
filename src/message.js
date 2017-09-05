5ef43242997f3008794eda5656d0f37b7e323a16
meddaoud <31484452+meddaoud@users.noreply.github.com>
2017-09-05 11:22:49 +0200

    Update message.js

--- src/message.js
+++ src/message.js
@@ -44,15 +44,7 @@ const replyMessage = (message) => {
     .then(() => {
       // Doconst recastai = require('recastai')
 
-const client = new recastai.request('7b38b88e159645c34bcdb90f77534738', 'en')
 
-client.converseText('hello')
-  .then(function(res) {
-    if (res.action) { console.log('Action: ', res.action.slug) }
-    const reply = res.reply()
-    console.log('Reply: ', reply)
-  }) some code after sending messages
-   
       
       
       
